8435e8b6eeba37699b53aa0bb2e7f38ff17b88ac
Ivan Vasilenko <ivan991015@gmail.com>
2020-04-13 21:18:11 +1000

    add all-in-one test

---
+++ PseudoJsTranslatorTests/tests/ast-building/valid/all-in-one/0000.js
@@ -0,0 +1,41 @@
+function foo(a, b) {
+    function f() {}
+    function g() { return }
+    {}
+    return function(x) { return a * x + b }
+}
+var bar = foo(2, 3)(5)
+var c = function() {}
+c()
+
+if (bar < 10 && false, foo === null, true) {
+    while ("What a nice place!", bar !== 0 || 2 + 2 === 4) {
+        if (bar <= 0)
+            continue
+        else {
+            bar = bar + 1 
+            break; 
+        }
+    }
+}
+
+var a = [, x, ]
+a = []
+a = [, , x, , y, , ]
+
+var o = {}
+o = {x : 42, while : "true", "x =" : 4, 2 : "is the answer", }
+o[2] = o.x + o['while']  
+delete x['true']
+delete o.x
+
+var x = 3, y, z = 5 + "In my heart I'm number."
+y = x + (z - y) * 5 / 2 % z >>> 2e7 >> x << y | x & y ^ y
+y = !y + ~y + +y + -y
+
+if (y < x);
+if (y > x);
+if (y <= x);
+if (y >= x);
+if (y === x);
+if (y !== x);
